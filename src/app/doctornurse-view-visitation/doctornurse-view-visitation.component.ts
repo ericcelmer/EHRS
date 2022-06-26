6aaef8a5ee63ec6777164182f47a203f5dd3724e
ericcelmer <eric.celmer@gmail.com>
2022-06-26 15:45:58 -0400

    commit

--- src/app/doctornurse-view-visitation/doctornurse-view-visitation.component.ts
+++ src/app/doctornurse-view-visitation/doctornurse-view-visitation.component.ts
@@ -20,10 +20,33 @@ export class DoctorNurseViewVisitationComponent implements OnInit {
 	}
 
 	getDataFromApi() {
+		/*this.visitationData = [
+			{
+				patientid:"37f068aa-fc08-4499-a049-babe85fe96bf",
+				visit_date:new Date("2022-05-13 11:24:46.408"),
+				visit_reason:"Covid vaccine shot",
+				symptoms:null,
+				visit_summary:null
+			},
+			{
+				patientid:"37f068aa-fc08-4499-a049-babe85fe96bf",
+				visit_date:new Date("2022-05-13 15:24:46.408"),
+				visit_reason:"Covid vaccine shot",
+				symptoms:null,
+				visit_summary:null
+			}
+		];
+		return;*/
 		//TODO replace with actual patient id
 		this.service.getVisitationHistory("37f068aa-fc08-4499-a049-babe85fe96bf").subscribe(data =>{
 			this.visitationData = data;
+			this.displayVisitationHistory();
+			console.log(this.visitationData);
 			//TODO what do i do with this
 		});
+	}
+	
+	displayVisitationHistory() {
+
 	}
 }
